c00605efaf114a65482b80e1672f296021b49e91
Tommy Azzino <tommy.azzino@gmail.com>
2021-06-19 15:58:08 -0400

    complete code to maneuver the xy tables in Cosmos

--- host-py/mmwsdr/xytable/xytable.py
+++ host-py/mmwsdr/xytable/xytable.py
@@ -22,6 +22,10 @@ class XYTable(object):
     def __init__(self, table_name):
         self.table = table_name  # either xytable1 or xytable2
         self.main_url = 'https://am1.cosmos-lab.org:5054/xy_table/'
+        self.xy_status = None
+        self.rotator_status = None
+        self.current_position = None
+        self.target_position = None
 
     def __del__(self):
         del self.table
@@ -52,6 +56,16 @@ class XYTable(object):
         else:
             # Success: print status of the xytable
             json_data = json.dumps(xmltodict.parse(r.content))
+            table_data = json_data['response']['action']['xy_table']
+            self.xy_status = table_data['@xy_status']
+            self.rotator_status = table_data['@rotator_status']
+            current_pos = table_data['current_position']
+            target_pos = table_data['target_position']
+            self.current_position = [current_pos['@x'], current_pos['@y'], current_pos['@angle']]
+            self.target_position = [target_pos['@x'], target_pos['@y'], target_pos['@angle']]
+            # print(f"The current position: {self.current_position}")
+            # print(f"The target position: {self.target_position}")
+            return self.xy_status, self.rotator_status, self.current_position, self.target_position
 
     def move(self, x, y, angle):
         # TODO: check that x,y, angle are valid inputs according to Cosmos specs
@@ -67,6 +81,16 @@ class XYTable(object):
         else:
             # Success: print new position of the xytable
             json_data = json.dumps(xmltodict.parse(r.content))
+            table_data = json_data['response']['action']['xy_table']
+            self.xy_status = table_data['@xy_status']
+            self.rotator_status = table_data['@rotator_status']
+            current_pos = table_data['current_position']
+            target_pos = table_data['target_position']
+            self.current_position = [current_pos['@x'], current_pos['@y'], current_pos['@angle']]
+            self.target_position = [target_pos['@x'], target_pos['@y'], target_pos['@angle']]
+            # print(f"The current position: {self.current_position}")
+            # print(f"The target position: {self.target_position}")
+            return self.xy_status, self.rotator_status, self.current_position, self.target_position
 
     def stop(self):
         params = {'name': self.table + '.sb1.cosmos-lab.org'}
@@ -78,3 +102,10 @@ class XYTable(object):
         else:
             # Success: print the current stop position of the xytable
             json_data = json.dumps(xmltodict.parse(r.content))
+            table_data = json_data['response']['action']['xy_table']
+            self.xy_status = table_data['@xy_status']
+            self.rotator_status = table_data['@rotator_status']
+            current_pos = table_data['current_position']
+            self.current_position = [current_pos['@x'], current_pos['@y'], current_pos['@angle']]
+            # print(f"The current position: {self.current_position}")
+            return self.xy_status, self.rotator_status, self.current_position
