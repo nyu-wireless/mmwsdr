36aece1f95fb68796110dc402782c9105f082637
Panagiotis Skrimponis <ps3857@nyu.edu>
2021-09-29 10:53:16 +0000

    update calibration and sounder projects

--- host/projects/cal/cal_iq_rx.py
+++ host/projects/cal/cal_iq_rx.py
@@ -5,10 +5,10 @@
 # Import Libraries
 import os
 import sys
-import numpy as np
 import argparse
+import numpy as np
 import matplotlib
-
+import math
 matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 
@@ -18,70 +18,12 @@ if not path in sys.path:
 import mmwsdr
 
 # Parameters
-sc = 400  # subcarrier index
-nread = 1024  # num of continuous samples per batch
-nskip = 1024 * 5  # num of samples to skip between batches
+nfft = 1024  # num of continuous samples per batch
+nskip = 1024*5  # num of samples to skip between batches
 nbatch = 100  # num of batches
-tx_pwr = 20000  # transmit power
 isdebug = True  # print debug messages
-
-
-def cal_rx(sdr0, freq):
-    """
-
-    :param sdr0:
-    :type sdr0:
-    :param freq:
-    :type freq:
-    :return:
-    :rtype:
-    """
-    sdr0.freq = freq  # set the receiver carrier frequency in Hz
-
-    # Receive data
-    rxtd = sdr0.recv(nread, nskip, nbatch)
-
-    rxfd = np.fft.fft(rxtd, axis=1)
-    rxfd = np.fft.fftshift(rxfd, axes=1)
-    f = np.linspace(-nread / 2, nread / 2)
-    for ibatch in range(nbatch):
-        plt.plot((abs(rxfd[ibatch, :])), '-')
-    plt.show()
-
-    np.savez_compressed('rxtd.npz', rxtd)
-
-
-def cal_tx(sdr0, freq):
-    """
-
-    :param sdr0:
-    :type sdr0:
-    :param freq:
-    :type freq:
-    :return:
-    :rtype:
-    """
-    nfft = nread  # num of FFT points
-    sdr0.freq = freq + 1.08e9  # set the transmitter carrier frequency in Hz
-
-    # Create the tx signal in frequency domain
-    txfd = np.zeros((nfft,), dtype='int16')
-    txfd[(nfft >> 1) - sc] = 1
-    txfd = np.fft.fftshift(txfd, axes=0)
-
-    # Then, convert it to time domain
-    txtd = np.fft.ifft(txfd, axis=0)
-
-    # Set the tx power
-    txtd = txtd / np.mean(np.abs(txfd)) * tx_pwr
-
-    # Transmit data
-    sdr0.send(txtd)
-
-    if sys.version_info[0] == 2:
-        ans = raw_input("Press enter to continue: ")
-    else:
-        ans = input("Press enter to continue: ")
+sc = 256  # subcarrier index
+tx_pwr = 12000  # transmit power
 
 
 def main():
@@ -93,34 +35,117 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--freq", type=float, default=60.48e9, help="receiver carrier frequency in Hz (i.e., 60.48e9)")
     parser.add_argument("--node", type=str, default='sdr2-in1', help="cosmos-sb1 node name (i.e., sdr2-in1)")
+    parser.add_argument("--mode", type=str, default='rx', help="sdr mode (i.e., rx)")
     args = parser.parse_args()
 
-    # Create an SDR object
+    if args.mode == 'tx':
+        args.freq = 61.29e9
+
+    # Create an SDR object and the XY table
     if args.node == 'sdr2-in1':
         sdr0 = mmwsdr.sdr.Sivers60GHz(ip='10.113.6.3', freq=args.freq, unit_name='SN0240', isdebug=isdebug)
+        xytable0 = mmwsdr.utils.XYTable('xytable1', isdebug=isdebug)
 
-        # Configure the FPGA
-        sdr0.fpga.configure('../../config/rfsoc.cfg')
-
-        # Make sure that the node is not transmitting any signal
-        sdr0.send(np.zeros((nread,), dtype='int16'))
-
-        cal_tx(sdr0, args.freq)
-        cal_rx(sdr0, args.freq)
+        # Move the SDR to the lower-right corner
+        xytable0.move(x=0, y=0, angle=0)
     elif args.node == 'sdr2-in2':
         sdr0 = mmwsdr.sdr.Sivers60GHz(ip='10.113.6.4', freq=args.freq, unit_name='SN0243', isdebug=isdebug)
+        xytable0 = mmwsdr.utils.XYTable('xytable2', isdebug=isdebug)
 
-        # Configure the FPGA
-        sdr0.fpga.configure('../../config/rfsoc.cfg')
-
-        # Make sure that the node is not transmitting any signal
-        sdr0.send(np.zeros((nread,), dtype='int16'))
-
-        cal_rx(sdr0, args.freq)
-        cal_tx(sdr0, args.freq)
+        # Move the SDR to the lower-left conrner
+        xytable0.move(x=1300, y=0, angle=0)
     else:
         raise ValueError("COSMOS node can be either 'sdr2-in1' or 'sdr2-in2'")
 
+    # Configure the RFSoC
+    sdr0.fpga.configure('../../config/rfsoc.cfg')
+
+    # Main loop
+    while (1):
+        if args.mode == 'tx':
+            # Create a signal in frequency domain
+            txfd = np.zeros((nfft,), dtype='complex')
+            txfd[(nfft >> 1) + sc] = 1
+            txfd = np.fft.fftshift(txfd, axes=0)
+
+            # Then, convert it to time domain
+            txtd = np.fft.ifft(txfd, axis=0)
+
+            # Set the tx power
+            txtd = txtd / np.max([np.abs(txtd.real), np.abs(txtd.imag)]) * tx_pwr
+
+            # Transmit data
+            sdr0.send(txtd)
+        elif args.mode == 'rx':
+            # Make sure that the node is not transmitting
+            # sdr0.send(np.zeros((nfft,), dtype='int16'))
+
+            # Receive data
+            rxtd = sdr0.recv(nfft, nskip, nbatch)
+
+            rxfd = np.fft.fft(rxtd, axis=1)
+            rxfd = np.fft.fftshift(rxfd, axes=1)
+
+            f = np.linspace(-nfft / 2, nfft / 2 - 1, nfft)
+            
+            print("Max subcarrier: {}".format(f[np.argmax(abs(rxfd[0,:]))]))
+            
+            sum_re = 0.0
+            sum_im = 0.0
+            sc = 166
+            for ibatch in range(nbatch):
+                fd = np.zeros((nfft,), dtype='complex')
+                fd[(nfft >> 1) + sc] = rxfd[ibatch, (nfft >> 1) + sc]
+                fd[(nfft >> 1) - sc] = rxfd[ibatch, (nfft >> 1) - sc]
+                fd = np.fft.fftshift(fd)
+                td = np.fft.ifft(fd)
+                sum_re += np.sqrt(np.mean(td.real**2))
+                sum_im += np.sqrt(np.mean(td.imag**2))
+
+            # Find the alpha
+            a = sum_re/sum_im
+            nvhypo = 101
+            vhypos = np.linspace(-1, 1, nvhypo)
+            re = (1/a)*rxtd.real.reshape(nbatch,nfft,1)
+            im = rxtd.real.reshape(nbatch,nfft,1)*(-1*np.tan(vhypos)/a) + rxtd.imag.reshape(nbatch,nfft,1)*(1/np.cos(vhypos))
+            td = re + 1j*im
+            fd = np.fft.fft(td, axis=1)
+            fd = np.fft.fftshift(fd, axes=1)
+            sbs = np.sum(np.abs(fd[:,(nfft >> 1) - sc,:]/fd[:,(nfft >> 1) + sc,:]), axis=0)
+            m = sbs / np.min(sbs)
+            plt.plot(vhypos, 20*np.log10(m))
+            plt.show()
+
+            v = vhypos[np.argmin(m)]
+            print('Alpha: {}'.format(a))
+            print('V: {}'.format(v))
+
+            td = rxtd[0,:]
+            re = (1/a)*td.real 
+            im = td.real*(-1*np.tan(v)/a) + td.imag*(1/np.cos(v))
+            td = re + 1j*im
+            
+            fd = np.fft.fftshift(np.fft.fft(td))
+            fd0 = np.fft.fftshift(np.fft.fft(rxtd[0,:]))
+            plt.plot(f, np.abs(fd0))
+            plt.plot(f, np.abs(fd))
+            plt.grid()
+            plt.show()
+
+            plt.plot(f, 20*np.log10(np.abs(fd0)))
+            plt.plot(f, 20*np.log10(np.abs(fd)))
+            plt.grid()
+            plt.show()
+        else:
+            raise ValueError("SDR mode can be either 'tx' or 'rx'")
+
+        if sys.version_info[0] == 2:
+            ans = raw_input("Enter 'q' to exit or\n press enter to continue ")
+        else:
+            ans = input("Enter 'q' to exit or\n press enter to continue ")
+
+        if ans == 'q':
+            break
     # Close the TPC connections
     del sdr0
 
---
+++ host/projects/chansounder/sounder.py
@@ -0,0 +1,125 @@
+"""
+
+"""
+
+# Import Libraries
+import os
+import sys
+import argparse
+import time
+
+import numpy as np
+import matplotlib
+
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+
+path = os.path.abspath('../../')
+if not path in sys.path:
+    sys.path.append(path)
+import mmwsdr
+
+# Parameters
+naoa = 5
+nfft = 1024  # num of continuous samples per batch
+nskip = 1024  # num of samples to skip between batches
+nbatch = 5  # num of batches
+isdebug = True  # print debug messages
+sc_min = -200  # min subcarrier index
+sc_max = 200  # max subcarrier index
+tx_pwr = 20000  # transmit power
+qam = (1 + 1j, 1 - 1j, -1 + 1j, -1 - 1j)
+
+# Find the angles of arrival
+aoa = np.linspace(-45,45,naoa)
+
+
+def main():
+    """
+
+    :return:
+    :rtype:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--freq", type=float, default=60.48e9, help="receiver carrier frequency in Hz (i.e., 60.48e9)")
+    parser.add_argument("--node", type=str, default='sdr2-in1', help="cosmos-sb1 node name (i.e., sdr2-in1)")
+    parser.add_argument("--mode", type=str, default='rx', help="sdr mode (i.e., rx)")
+    args = parser.parse_args()
+
+    # Create an SDR object and the XY table
+    if args.node == 'sdr2-in1':
+        sdr0 = mmwsdr.sdr.Sivers60GHz(ip='10.113.6.3', freq=args.freq, unit_name='SN0240', isdebug=isdebug)
+        xytable0 = mmwsdr.utils.XYTable('xytable1', isdebug=isdebug)
+
+        # Move the SDR to the lower-right corner
+        xytable0.move(x=0, y=0, angle=0)
+    elif args.node == 'sdr2-in2':
+        sdr0 = mmwsdr.sdr.Sivers60GHz(ip='10.113.6.4', freq=args.freq, unit_name='SN0243', isdebug=isdebug)
+        xytable0 = mmwsdr.utils.XYTable('xytable2', isdebug=isdebug)
+
+        # Move the SDR to the lower-left conrner
+        xytable0.move(x=1300, y=0, angle=0)
+    else:
+        raise ValueError("COSMOS node can be either 'sdr2-in1' or 'sdr2-in2'")
+
+    # Configure the RFSoC
+    sdr0.fpga.configure('../../config/rfsoc.cfg')
+
+    np.random.seed(0)
+    
+    # Create a signal in frequency domain
+    txfd = np.zeros((nfft,), dtype='complex')
+    txfd[((nfft >> 1) + sc_min):((nfft >> 1) + sc_max)] = np.random.choice(qam, len(range(sc_min, sc_max)))
+    txfd = np.fft.fftshift(txfd, axes=0)
+    
+    # Then, convert it to time domain
+    txtd = np.fft.ifft(txfd, axis=0)
+
+    # Main loop
+    while (1):
+        if args.mode == 'tx':
+            # Set the tx power
+            txtd = txtd / np.max([np.abs(txtd.real), np.abs(txtd.imag)]) * tx_pwr
+
+            # Transmit data
+            sdr0.send(txtd)
+        elif args.mode == 'rx':
+            # Make sure that the nodes are not transmitting
+            # sdr0.send(np.zeros((nfft,), dtype='int16'))
+            # Receive data
+            for iaoa in range(naoa):
+                # set AoA
+                xytable0.move(x=1300, y=0, angle=aoa[iaoa])
+                # sdr0.beam_index = iaoa
+                time.sleep(1)
+
+                # Receive data
+                rxtd = sdr0.recv(nfft, nskip, nbatch)
+                rxfd = np.fft.fft(rxtd, axis=1)
+                rxfd = np.fft.fftshift(rxfd, axes=1)
+
+                corrfd = rxfd * np.conj(txfd)
+                corrtd = np.fft.ifft(corrfd, axis=1)
+                mag = np.abs(corrtd)
+                for it in range(nbatch):
+                    plt.plot(mag[it,:]/np.max(mag[it,:]))
+                plt.show()
+        else:
+            raise ValueError("SDR mode can be either 'tx' or 'rx'")
+
+        if sys.version_info[0] == 2:
+            ans = raw_input("Enter 'q' to exit or\n press enter to continue ")
+        else:
+            ans = input("Enter 'q' to exit or\n press enter to continue ")
+
+        if ans == 'q':
+            break
+    # Close the TPC connections
+    del sdr0
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
